b002e31b32017c445d8f3dfc134d73be49152b00
Nati <natigoel15@gmail.com>
2016-04-06 16:10:28 +0300

    push from nati

---
+++ toposort-app/src/main/java/cs/technion/ac/il/sd/app/ToposortImpl.java
@@ -0,0 +1,14 @@
+package cs.technion.ac.il.sd.app;
+
+import java.io.File;
+
+/**
+ * Created by Nati on 06/04/2016.
+ */
+public class ToposortImpl implements Toposort
+{
+    @Override
+    public void processFile(File file) {
+
+    }
+}
